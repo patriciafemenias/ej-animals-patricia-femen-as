eb022cdb03021ffa92d824e08773fa171c72e73a
Patricia <patricia.femenias@gmail.com>
2023-09-22 12:15:41 +0200

    Add Animals objects

---
+++ index.js
@@ -0,0 +1,17 @@
+const animalA = {
+  name: "Pepito",
+  age: 130,
+  weight: 160,
+  species: "dinosaur",
+  extinctionYear: 200,
+  id: 1,
+};
+
+const animalB = {
+  name: "Manolito",
+  age: 240,
+  weight: 400,
+  species: "turtle",
+  extinctionYear: 360,
+  id: 2,
+};
